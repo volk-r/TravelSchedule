b02c88498c162751cbdafaed753336f7fccb63cc
Roman Romanov <rromanov.vk@gmail.com>
2024-10-31 14:46:09 +0500

    intial

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Roman Romanov on 31.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
