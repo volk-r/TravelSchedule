3081a7dedad6729d5e5d688aed9a51ba8efa9bb2
Roman Romanov <rromanov.vk@gmail.com>
2024-11-09 22:07:47 +0500

    clean up

--- TravelSchedule/Services/NearestStationsService.swift
+++ TravelSchedule/Services/NearestStationsService.swift
@@ -10,27 +10,6 @@ import Foundation
 import OpenAPIRuntime
 import OpenAPIURLSession
 
-/*
- // TODO: example
- 
- func stations() {
-     let client = Client(
-         serverURL: try Servers.server1(),
-         transport: URLSessionTransport()
-     )
-     
-     let service = NearestStationsService(
-         client: client,
-         apikey: "ВАШ КЛЮЧ"
-     )
-     
-     Task {
-         let stations = try await service.getNearestStations(lat: 59.864177, lng: 30.319163, distance: 50)
-         print(stations)
-     }
- }
- */
-
 final class NearestStationsService: NearestStationsServiceProtocol {
     private let client: Client
     private let apikey: String
